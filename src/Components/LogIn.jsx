721f4a94a0c0fe32d2b513002a77d6959497211c
Kyle <kylemorris8484@gmail.com>
2025-03-23 19:56:14 +0000

    Improve styling and add react hook form

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,20 +1,29 @@
+import { useForm } from "react-hook-form"
+
 export default function LogIn() {
+
+    const {register, handleSubmit , formState: {errors} } = useForm()
+    const onSubmit = (data) => console.log(data)
+
     return (
-        <div class="flex flex-col bg-gray-100 rounded-md mx-100 mt-20 items-center gap-2 py-5">
+        <form 
+            className="flex flex-col bg-gray-100 rounded-md mx-auto w-100 mt-20 items-center gap-2 py-5"
+            onSubmit={handleSubmit(onSubmit)}
+        >
 
-            <h2 class="text-3xl text-center pb-8">Log In</h2>
+            <h2 className="text-3xl text-center pb-8">Log In</h2>
 
-            <input placeholder="Email" class="border border-gray-600 w-75 p-1"></input>
-            <input type="password" placeholder="Password" class="border border-gray-600 w-75 p-1 mt-3"></input>
+            <input type="email" placeholder="Email" className="border border-gray-600 w-75 p-1" {...register("email")} required />
+            <input type="password" placeholder="Password" className="border border-gray-600 w-75 p-1 mt-3" {...register("password")} required minLength={8} />
             
-            <div class="flex items-center mt-4">
-                <input id="remember-me" type="checkbox" class="mr-1.5"></input>
-                <label for="remember-me" class="pb-0.5">Remember Me</label>
+            <div className="flex items-center mt-4">
+                <input id="remember-me" type="checkbox" className="mr-1.5" {...register("remember-me")}></input>
+                <label htmlFor="remember-me" className="pb-0.5">Remember Me</label>
             </div>
 
-            <button class="mt-8 border px-5 py-1 bg-blue-500 text-white">Log in</button>
+            <input type="submit" className="mt-8 border px-5 py-1 bg-blue-500 text-white" value="Log In" />
 
-            <p class="text-xs">Dont have an account? <a href="/" class="underline">Sign up</a></p>
-        </div>
+            <p className="text-xs">Dont have an account? <a href="/" className="underline">Sign up</a></p>
+        </form>
     )
 }
