e26840fbcd9c81828fed322f1f8debd17866ad1f
Kyle <kylemorris8484@gmail.com>
2025-03-23 21:40:47 +0000

    Add confetti

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,9 +1,15 @@
 import { useForm } from "react-hook-form"
+import { useState } from "react"
+import Realistic from "react-canvas-confetti/dist/presets/fireworks";
 
 export default function LogIn() {
 
     const {register, handleSubmit , formState: {errors} } = useForm()
-    const onSubmit = (data) => console.log(data)
+    const [submitSuccess, setSubmitSuccess] = useState(false)
+    
+    const onSubmit = (data) => {
+        setSubmitSuccess(!submitSuccess)
+    }
 
     return (
         <form 
@@ -21,9 +27,10 @@ export default function LogIn() {
                 <label htmlFor="remember-me" className="pb-0.5">Remember Me</label>
             </div>
 
-            <input type="submit" className="mt-8 border px-5 py-1 bg-blue-500 text-white" value="Log In" />
+            <input type="submit" className="mt-4 border px-5 py-1 bg-blue-500 hover:bg-blue-400 text-white transition" value="Log In" />
 
             <p className="text-xs">Dont have an account? <a href="/" className="underline">Sign up</a></p>
+            { submitSuccess && <Realistic autorun={{speed: 1, duration: 1}}/> }
         </form>
     )
 }
